c0352e71aa4f58478d32bc36f6d0bb823e650d86
wahab <wahabrasool08@gmail.com>
2024-08-31 22:22:41 +0500

    space bar

---
+++ app.js
@@ -0,0 +1,21 @@
+var hits = 0;
+var hitElement = document.querySelector( '.hits' );
+document.body.onkeyup = function(e) {
+  if( e.keyCode == 32 ) {
+    addHit();
+  }
+}
+
+var addHit = function() {
+  hits++;
+  renderHits();
+}
+
+var renderHits = function() {
+  hitElement.innerHTML = hits;
+}
+
+var resetHits = function() {
+  hits = 0;
+  renderHits();
+}
